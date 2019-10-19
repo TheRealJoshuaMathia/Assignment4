ed1f072268bc9dd85068bc3bae191378469374df
parkerg44 <56697192+parkerg44@users.noreply.github.com>
2019-10-18 22:20:53 -0700

    Update Board.h

--- Board.h
+++ Board.h
@@ -39,7 +39,7 @@ public:
 	~Board();					//destructor
 	bool insert(int playerID, int xPos, int yPos);
 	bool remove(int playerID);
-	bool find(int playerID);
+	bool find1(int playerID);
 	bool moveTo(int playerID, int newxPos, int newypos);
 	void printbyID();
 	bool checkInsertion(int xPos, int yPos);
@@ -47,16 +47,16 @@ public:
 
 	void insertPair(int xPos, int yPos);
 	bool removePair(int xPos, int yPos);
-	void getPair(int playerID, int *xPos, int *yPos);
+	void getPair(int playerID, int* xPos, int* yPos);
 
 
 private:
 	//multimap<Player> gameboard;			QUESTION: What type of tree do we want to use here? Set, Map , mulit-set,map
 	//want a tree of player values 
-	map<int,Player> gameBoard;		//most likely need to change			//defines set tree of players for housing values 
+	map<int, Player> gameBoard;		//most likely need to change			//defines set tree of players for housing values 
 	int N; //number of players on the board
 	int M; //size of the board 
-	map <int,Player>::iterator itr;
+	map <int, Player>::iterator itr;
 	multimap <int, int> trackPairs; // Checks list of current (x,y) positions
 	multimap <int, int>::iterator pairIT;
 };
