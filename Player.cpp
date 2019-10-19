e583b501be643ea667df3531de24eaebcbd46936
parkerg44 <56697192+parkerg44@users.noreply.github.com>
2019-10-18 22:21:40 -0700

    Update Player.cpp

--- Player.cpp
+++ Player.cpp
@@ -2,39 +2,39 @@
 
 Player::Player(int playerID, int xPos, int yPos)
 {
-    this->playerID = playerID;
-    this->xPos = xPos;
-    this->yPos = yPos;
+	this->playerID = playerID;
+	this->xPos = xPos;
+	this->yPos = yPos;
 }
 
 Player::~Player()
 {
-    //Destructor
+	//Destructor
 }
 int Player::getID()const
 {
-    return this->playerID;
+	return this->playerID;
 }
 int Player::getxPos()const
 {
-    return this->xPos;
+	return this->xPos;
 }
 int Player::getyPos()const
 {
-    return this->yPos;
+	return this->yPos;
 }
 
 void Player::setID(int newID)
 {
-    this->playerID = newID;
+	this->playerID = newID;
 }
 
 void Player::setxPos(int newxPos)
 {
-    this->xPos = newxPos;
+	this->xPos = newxPos;
 }
- 
+
 void Player::setyPos(int newyPos)
 {
-    this->yPos = newyPos;
+	this->yPos = newyPos;
 }
